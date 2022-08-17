b6acd4baa899f07f4a5c5798443455d515734404
Rosendo Martinez <rosendomartinez270@gmail.com>
2022-08-16 19:44:37 -0500

    Fix gitpages bug??

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ const API_KEY = '89ce0b8619fbcff23bebdba6f0624f9f';
 const CORS = {mode: 'cors'};
 
 async function getGeoCoordinatesOfCity(city) {
-    let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`, CORS);
+    let response = await fetch(`HTTPS://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`, CORS);
     if (response.ok) {
         let apiData = await response.json();
         const cityData = {lat: apiData[0].lat, lon: apiData[0].lon};
@@ -14,7 +14,7 @@ async function getGeoCoordinatesOfCity(city) {
 
 async function getCurrentWeatherOfCity(city) {
     let cityData = await getGeoCoordinatesOfCity(city);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${cityData.lat}&lon=${cityData.lon}&units=metric&appid=${API_KEY}`;
+    const url = `HTTPS://api.openweathermap.org/data/2.5/weather?lat=${cityData.lat}&lon=${cityData.lon}&units=metric&appid=${API_KEY}`;
     let response = await fetch(url, CORS);
     if (response.ok) {
         let apiData = await response.json();
